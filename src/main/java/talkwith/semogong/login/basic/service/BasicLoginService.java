0632241d707b9e96cf93a7c1eba00b814f4f67ad
bob8dod <bob8dod@gmail.com>
2023-09-11 21:40:14 +0900

    edit error

--- src/main/java/talkwith/semogong/login/basic/service/BasicLoginService.java
+++ src/main/java/talkwith/semogong/login/basic/service/BasicLoginService.java
@@ -2,7 +2,7 @@ package talkwith.semogong.login.basic.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface BasicLoginService {
 
     String test();
--- src/main/java/talkwith/semogong/login/basic/service/impl/BasicLoginServiceImpl.java
+++ src/main/java/talkwith/semogong/login/basic/service/impl/BasicLoginServiceImpl.java
@@ -1,7 +1,12 @@
 package talkwith.semogong.login.basic.service.impl;
 
+import org.springframework.stereotype.Service;
 import talkwith.semogong.login.basic.service.BasicLoginService;
 
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class BasicLoginServiceImpl implements BasicLoginService {
     @Override
     public String test() {
